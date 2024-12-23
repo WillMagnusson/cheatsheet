d931a66f29b11897bc11ff914896311266e8e47a
WillMagnusson <107117825+WillMagnusson@users.noreply.github.com>
2024-12-23 17:05:50 +0100

    Update typescriptCheats.ts

--- typescriptCheats.ts
+++ typescriptCheats.ts
@@ -7,34 +7,36 @@ const uniqueGlobalAreas = globalAreas?.filter(
 );
 
 //Add new object and scroll into view
-let npId;
-const createNewParameter = async () => {
-  debounce(() => {
-    const formData = form.getValues();
-    const newParameter: ParameterInstance = {
-      ...formData,
-      id: uuid(),
-    } as ParameterInstance;
+//dependant on the elements getting the Id as id attribute
+  const createNewParameter = async () => {
+    let npId: string;
+    let npCat: string;
+    debounce(() => {
+      const formData = form.getValues();
+      const newParameter: ParameterInstance = {
+        ...formData,
+        id: uuid(),
+      } as ParameterInstance;
 
-    npId = newParameter.id;
+      npId = newParameter.id;
+      npCat = newParameter.category;
 
-    snapshot &&
-      setSnapshot({
-        ...snapshot,
-        parameters: [...(snapshot.parameters ?? []), newParameter],
-      });
+      snapshot &&
+        setSnapshot({
+          ...snapshot,
+          parameters: [...(snapshot.parameters ?? []), newParameter],
+        });
+    }, 1000);
 
-    //capitalize since state values of categories are capitalized for some reason
-    const category =
-      formData.category.charAt(0).toUpperCase() + formData.category.slice(1);
-    //open category accordion
-    setOpenStates((prev: { [name: string]: boolean }) => ({
-      ...prev,
-      [category]: true,
-    }));
-
-    if (npId)
-      document.getElementById(npId)?.scrollIntoView({ behavior: 'smooth' });
-  }, 1000);
-  setShowDialog(false);
-};
+    //wait a moment for the data to update and the new parameter to be rendered
+    //if the new param is the first of its category, scroll to the category since its closed
+    setTimeout(() => {
+      if (npId) {
+        document.getElementById(npId)?.scrollIntoView({ behavior: 'smooth' });
+      } else {
+        document.getElementById(npCat)?.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 1500);
+    setShowDialog(false);
+    setShowRecommendation(false);
+  };
