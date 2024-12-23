315504693175f3996a21c77a9d59ba7c0698ec8f
WillMagnusson <107117825+WillMagnusson@users.noreply.github.com>
2024-12-23 16:34:29 +0100

    Update typescriptCheats.ts

--- typescriptCheats.ts
+++ typescriptCheats.ts
@@ -9,32 +9,32 @@ const uniqueGlobalAreas = globalAreas?.filter(
 //Add new object and scroll into view
 let npId;
 const createNewParameter = async () => {
-    debounce(() => {
-      const formData = form.getValues();
-      const newParameter: ParameterInstance = {
-        ...formData,
-        id: uuid(),
-      } as ParameterInstance;
+  debounce(() => {
+    const formData = form.getValues();
+    const newParameter: ParameterInstance = {
+      ...formData,
+      id: uuid(),
+    } as ParameterInstance;
 
-      npId = newParameter.id;
+    npId = newParameter.id;
 
-      snapshot &&
-        setSnapshot({
-          ...snapshot,
-          parameters: [...(snapshot.parameters ?? []), newParameter],
-        });
+    snapshot &&
+      setSnapshot({
+        ...snapshot,
+        parameters: [...(snapshot.parameters ?? []), newParameter],
+      });
 
-      //capitalize since state values of categories are capitalized for some reason
-      const category =
-        formData.category.charAt(0).toUpperCase() + formData.category.slice(1);
-      //open category accordion
-      setOpenStates((prev: { [name: string]: boolean }) => ({
-        ...prev,
-        [category]: true,
-      }));
+    //capitalize since state values of categories are capitalized for some reason
+    const category =
+      formData.category.charAt(0).toUpperCase() + formData.category.slice(1);
+    //open category accordion
+    setOpenStates((prev: { [name: string]: boolean }) => ({
+      ...prev,
+      [category]: true,
+    }));
 
-      if (npId)
-        document.getElementById(npId)?.scrollIntoView({ behavior: 'smooth' });
-    }, 1000);
-    setShowDialog(false);
+    if (npId)
+      document.getElementById(npId)?.scrollIntoView({ behavior: 'smooth' });
+  }, 1000);
+  setShowDialog(false);
 };
